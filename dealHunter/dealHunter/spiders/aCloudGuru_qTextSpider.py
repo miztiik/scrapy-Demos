06b52239bbe21aebdc66f9979860d32c054d8f40
Mystique <b4wt@quantumfoam.uni.cc>
2016-11-02 12:46:28 +0530

    Lots of improvements, - Ability to read urls from file and scrap them - Each URL output is dumped to separate file with tag and date prefix
    Working code can be re-used as is.
    
    Signed-off-by: Mystique <b4wt@quantumfoam.uni.cc>

--- dealHunter/dealHunter/spiders/aCloudGuru_qTextSpider.py
+++ dealHunter/dealHunter/spiders/aCloudGuru_qTextSpider.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pdb, time
+import pdb, json, os
 
-# Imports for Scrapy
-from scrapy.spiders import Spider
-from scrapy.item import Item, Field
+from datetime import datetime, date, time
 
 # Imports for virtual display
 from xvfbwrapper import Xvfb
@@ -17,34 +15,37 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 
-
-import json
+# Imports for Scrapy spider
+from scrapy.spiders import Spider
 
 class aCloudGuru_qTextSpider(Spider):
-    name = "aCloudGuru_qTextSpider"
+    #name = "aCloudGuru_qTextSpider"
+    name = "tspidy"
     allowed_domains = [ "acloud.guru" ]
 
-    def __init__(self):
+    def __init__(self, srcLnksJson=None):
+        self.srcLnksJson = srcLnksJson
         self.start_urls = [ "http://www.google.co.in" ]
 
-    def parse(self, response):
+    def parse(self,response):
 
         self.setUpBrowser()
 
-        uriData = {}
+        if self.srcLnksJson:
+            with open( self.srcLnksJson , 'r' ) as f:
+                uriData = json.load(f)
+        else:
 
-        uriData['s3']   = { 'awsTag' : 's3' , 'sourceUrl' : 'https://acloud.guru/course/aws-certified-solutions-architect-professional/discuss/-KPOrhtqq52941y0Iy6A/choose-2-answers-vpc' ,  'crawled': 'False', 'pgCrawled' : 0 , 'pageLoadWaitTime' : 60 }
-        uriData['elb']  = { 'awsTag' : 'elb' , 'sourceUrl' : 'https://acloud.guru/course/aws-certified-solutions-architect-professional/discuss/-KAmC01FWYI1WnCgpWaT/elb-design-patterns' ,  'crawled': 'False', 'pgCrawled' : 0 , 'pageLoadWaitTime' : 60 }
+            print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            print "   Source Json file not mentioned, continuing with defaults, usage is as below"
+            print "\n   scrapy crawl " + self.name + " -a srcLnksJson=<filename> "
+            print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+            uriData = { 'awsTag' : 's3' ,"sourceUrl" : "https://acloud.guru/forums/all/s3", 'uri' : ['https://acloud.guru/course/aws-certified-solutions-architect-professional/discuss/-KPOrhtqq52941y0Iy6A/choose-2-answers-vpc'] ,  'crawled': 'False', 'pgCrawled' : '0' , 'pageLoadWaitTime' : '60' }
 
-        dataDump = {}
 
-        for key in uriData.iterkeys():
-            # dataDump = self.collectQueryText(uriData['s3'])
-            # self.writeToFile(dataDump)
-            self.writeToFile( self.collectQueryText( uriData[key] ) )
-
-        # dataDump = self.collectQueryText(uriData['elb'])
+        self.collectQueryText( uriData )
 
         self.tearDownBrowser()
 
@@ -75,8 +76,6 @@ class aCloudGuru_qTextSpider(Spider):
     Function to collect the Urls in a given page
     """
     def collectQueryText(self,uriDict):
-        # The time to wait for the webpage to laod in seconds
-        
 
         ## The XPATH ID of the element for which the the page load waits before processing other requests
         ec_XPATH        = "//div[@class='card discussion-card']/div[@class='discussion-card-title loaded']/h2"
@@ -85,53 +84,80 @@ class aCloudGuru_qTextSpider(Spider):
         qTopic_XPATH    = "//div[@class='w-full ui-select-container ui-select-multiple ui-select-bootstrap dropdown form-control ng-valid']/div/span[@class='ui-select-match']/span"
      
 
-        self.driver.set_page_load_timeout(uriDict['pageLoadWaitTime'])
-        self.driver.get(uriDict['sourceUrl'])
+        self.driver.set_page_load_timeout( int(uriDict['pageLoadWaitTime']) )
 
-        # time.sleep(uriDict['pageLoadWaitTime'])
-
-        try:
-            # pdb.set_trace()
-            # element_present_check_1 = WebDriverWait(self.driver, uriDict['pageLoadWaitTime']).until(EC.presence_of_all_elements_located((By.XPATH, ec_XPATH)))
-            element_present_check_1 = WebDriverWait(self.driver, uriDict['pageLoadWaitTime']).until(EC.presence_of_element_located((By.XPATH, ec_XPATH)))
-            qTexts = self.driver.find_elements_by_xpath(qText_XPATH)
-            qAnswers = self.driver.find_elements_by_xpath(qAnswer_XPATH)
-            qTopics = self.driver.find_elements_by_xpath(qTopic_XPATH)
-
+        for lnkIndex,lnk in enumerate( uriDict['uri'] ):
+            dataDump = {}
+            dataDump['sourceUrl'] = lnk
             
-            questionTxt = []
-            for para in qTexts:
-                questionTxt.append( para.text )
-            uriDict['Question'] = questionTxt
-            
-            allAnswers = {}
+            # Split the link from the end to get the title and use only the second part[1]
+            dataDump['Title'] = lnk.rsplit("/",1)[1]
 
-            for index, ans in enumerate(qAnswers, 1):
-                answerTxt = []
-                ansPara = ans.find_elements_by_xpath('.//p')
-                for para in ansPara:
-                    answerTxt.append( para.text )
-                allAnswers['usr-{0}'.format(index)] = answerTxt
-            uriDict['Answers'] = allAnswers
+            # time.sleep( int( uriDict['pageLoadWaitTime']) )
+    
+            try:
+                #pdb.set_trace()
+                self.driver.get( lnk )
 
-            questionTags = []
-            for qTag in qTopics:
-                # print qTag.get_attribute('innerHTML')
-                # print qTag.text
-                questionTags.append( qTag.text )
-            uriDict['Tags'] = questionTags
+                # pdb.set_trace()
+                # element_present_check_1 = WebDriverWait(self.driver, uriDict['pageLoadWaitTime']).until(EC.presence_of_all_elements_located((By.XPATH, ec_XPATH)))
+                element_present_check_1 = WebDriverWait(self.driver, int(uriDict['pageLoadWaitTime'])).until(EC.presence_of_element_located((By.XPATH, ec_XPATH)))
+                qTexts = self.driver.find_elements_by_xpath(qText_XPATH)
+                qAnswers = self.driver.find_elements_by_xpath(qAnswer_XPATH)
+                qTopics = self.driver.find_elements_by_xpath(qTopic_XPATH)
+    
+                
+                questionTxt = []
+                for para in qTexts:
+                    questionTxt.append( para.text )
+                dataDump['Question'] = questionTxt
+                
+                allAnswers = {}
+    
+                for ansIndex, ans in enumerate(qAnswers, 1):
+                    answerTxt = []
+                    ansPara = ans.find_elements_by_xpath('.//p')
+                    for para in ansPara:
+                        answerTxt.append( para.text )
+                    allAnswers['usr-{0}'.format(ansIndex)] = answerTxt
+                dataDump['Answers'] = allAnswers
+    
+                questionTags = []
+                for qTag in qTopics:
+                    # print qTag.get_attribute('innerHTML')
+                    # print qTag.text
+                    questionTags.append( qTag.text )
+                dataDump['Tags'] = questionTags
 
-            uriDict['pgCrawled'] = str( uriDict['pgCrawled'] + 1)
-            uriDict['crawled'] = "True"
 
-        except TimeoutException:
-            self.driver.execute_script("window.stop();")
-            print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            print "           THE PAGE DID NOT LOAD PROPERLY         "
-            print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                dataDump['awsTag'] = uriDict['awsTag']
+                # dataDump['pgCrawled'] = str( int(uriDict['pgCrawled']) + 1)
+                dataDump['pgCrawled'] = "1"
+                dataDump['crawled'] = "True"
+                dataDump['dateScraped'] = date.today().strftime("%Y-%m-%d") + "-" + datetime.now().strftime('%H-%M')
+                dataDump['pageLoadWaitTime'] = uriDict['pageLoadWaitTime']
+    
+                print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print "  SUCCESSFULLY SCRAPPED : {0} of {1}, DUMPING DATA".format( lnkIndex , len(uriDict['uri']))
+                print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+    
+                self.writeToFile( dataDump )
+    
+            except TimeoutException:
+                self.driver.execute_script("window.stop();")
+                print "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                print " TIMEOUT exceptions, THE PAGE DID NOT LOAD PROPERLY         "
+                print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 
-        return uriDict
+        return None
 
     def writeToFile(self, dataDump):
-        with open('acloudguru-{0}-{1}.json'.format( dataDump['awsTag'] , dataDump['Tags'][0] ), 'w') as f:
+        with open('{0}-acloudguru-{1}-{2}.json'.format( dataDump['dateScraped'] , dataDump['awsTag'] , dataDump['Tags'][0] ), 'w') as f:
             json.dump(dataDump, f, indent=4,sort_keys=True)
+
+
+# def main():
+#     aCloudGuru_qTextSpider()
+# 
+# if __name__ == "__main__":
+#     main()
